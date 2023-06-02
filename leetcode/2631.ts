efdc6c88a19fd6007b1d26d39962a9ad420d5eb1
JSH-data <super60447@gmail.com>
2023-06-02 22:40:06 +0900

    2023-06-03

---
+++ leetcode/2631.ts
@@ -0,0 +1,19 @@
+declare global {
+    interface Array<T> {
+        groupBy(fn: (item: T) => string): Record<string, T[]>
+    }
+}
+
+Array.prototype.groupBy = function (fn) {
+    const obj = {}
+    for (let i = 0; i < this.length; i++) {
+        const key = fn(this[i])
+        if (obj[key]) obj[key].push(this[i])
+        else obj[key] = [this[i]]
+    }
+    return obj
+}
+
+/**
+ * [1,2,3].groupBy(String) // {"1":[1],"2":[2],"3":[3]}
+ */
