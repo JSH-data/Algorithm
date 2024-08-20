ff1460bc454078c3c85b807d3401ac780fea19bc
SeungHwa Jeong <62323657+JSH-data@users.noreply.github.com>
2024-08-20 16:17:52 +0900

    Update Algorithm 순위.js from programmers

--- programmers/순위.js
+++ programmers/순위.js
@@ -1,25 +1,19 @@
 function solution(n, results) {
-    // [[winSet, loseSet]]
-    const result = Array.from({length: n + 1}, () => [new Set(), new Set()]);
+    const records = Array.from({length: n + 1}, () => [new Set(), new Set()]);
     
     for(const [a, b] of results) {
-        result[a][0].add(b);
-        result[b][1].add(a);
+        records[a][0].add(b);
+        records[b][1].add(a);
     }
     
     for(let i = 1; i <= n; i += 1) {
-        const winners = result[i][1]; // 이긴 사람들
-        const loosers = result[i][0]; // 진 사람들
+        const loosers = records[i][0]; // i가 이긴 사람들
+        const winners = records[i][1]; // i를 이긴 사람들
         
         for(const win of winners) {
-            for(const loose of loosers) {
-                result[win][0].add(loose);
-            }
-        }
-        
-        for(const lose of loosers) {
-            for(const win of winners) {
-                result[lose][1].add(win);
+            for(let loose of loosers) {
+                records[win][0].add(loose);
+                records[loose][1].add(win);
             }
         }
     }
@@ -27,7 +21,7 @@ function solution(n, results) {
     let answer = 0;
     
     for(let i = 1; i <= n; i += 1) {
-        if(result[i][0].size + result[i][1].size === n - 1) {
+        if(records[i][0].size + records[i][1].size === n - 1) {
             answer += 1;
         }
     }
