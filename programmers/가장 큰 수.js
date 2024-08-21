3e8cd481c23683e0954ae9a7f188d794221c186d
SeungHwa Jeong <62323657+JSH-data@users.noreply.github.com>
2024-08-21 17:36:43 +0900

    Update Algorithm 가장 큰 수.js from programmers

--- programmers/가장 큰 수.js
+++ programmers/가장 큰 수.js
@@ -6,24 +6,7 @@ function solution(numbers) {
         return second - first;
     });
     
-    let flag = true;
-    let answer = '';
+    const answer = numbers.join("");
     
-    for(let i = 0; i < numbers.length; i += 1) {
-        if(numbers[i] === 0 && flag) {
-            continue;
-        } else {
-            if(flag) {
-                flag = false;
-            }
-            
-            answer += numbers[i];
-        }
-    }
-    
-    if(answer === "") {
-        return "0";
-    }
-    
-    return answer;
+    return Number(answer) === 0 ? "0" : answer;
 }
